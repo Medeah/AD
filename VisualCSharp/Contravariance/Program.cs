f9eb8caee086c80739f2d7c051327eab93613a55
Frederik Andersen <fhan13@student.aau.dk>
2014-11-12 23:56:36 +0100

    Contravariance

---
+++ VisualCSharp/Contravariance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contravariance
+{
+    class Sodavand { }
+
+    class Cola : Sodavand { }
+
+    // Contravariant interface. 
+    interface C<in T>  {
+        void doStuff(T para);
+    }
+
+    class D<T> : C<T>
+    {
+        public void doStuff(T para)
+        {
+        }
+    }
+
+    class E : C<Sodavand>
+    {
+        public void doStuff(Sodavand para)
+        {
+        }
+    }
+
+    class F
+    {
+        public virtual void doStuff(Cola a) { }
+    }
+    class G : F
+    {
+
+        public void doStuff(Sodavand a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void doStuff(Cola a)
+        {
+            doStuff((Sodavand)a);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C<Cola> test = new D<Sodavand>();
+            C<Cola> test1 = new E();
+            //E<Object> test = new E<String>();
+            Console.WriteLine("hej");
+            Console.ReadLine();
+        }
+    }
+}
